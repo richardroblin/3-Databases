e4145c909fd38a373b04b11e5645d1db94e69aa9
Richard Roblin <richard.roblin@outlook.com>
2023-02-09 16:52:38 +0000

    commit changes before laptop return

--- Student_Directory/students_table.sql
+++ Student_Directory/students_table.sql
@@ -1,5 +1,9 @@
 CREATE TABLE students (
   id SERIAL PRIMARY KEY,
   name text,
-  cohort int
+-- The foreign key name is always {other_table_singular}_id
+  cohort_id int,
+  constraint fk_cohort foreign key(cohort_id)
+    references cohorts(id)
+    on delete cascade
 );
